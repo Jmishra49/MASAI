54d15b013d098388059d078006b6aa2e7f1ac4f6
Jmishra49 <mishrajyotiraditya49@gmail.com>
2025-04-13 23:34:24 +0530

    Your commit message

---
+++ FakeStore/firebase.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "YAIzaSyBxiRH0gQ6sNi6R7kBLvf6gL1xHcT7zD0g",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "YOUR_DATABASE_URL",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
